b520c2568db463b766524cbeb35573955f833ca1
Lati <l4tt1haga@gmail.com>
2025-01-17 19:25:17 +0100

    adding simple server

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/latt3y/cv-builder
 
-go 1.22.6
+go 1.23.4
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
-package cvbuilder
+package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "io"
+  "errors"
+  "os"
+)
+
+func get_root(w http.ResponseWriter, r *http.Request) {
+  fmt.Printf("got / request");
+  io.WriteString(w, "This is my website!\n");
+}
+
+func get_hello(w http.ResponseWriter, r *http.Request) {
+  fmt.Printf("get /hello request");
+  io.WriteString(w, "Hello HTTP");
+}
 
 func main() {
-  fmt.Println("Hello from Cv builder");
+  http.HandleFunc("/", get_root);
+  http.HandleFunc("/hello", get_hello);
+
+  err := http.ListenAndServe(":3333", nil);
+
+  if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
